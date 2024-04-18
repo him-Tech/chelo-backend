50f52d34fbf1be90007f132dd520423f2f552a56
Harsh <61960651+harshsindhu0408@users.noreply.github.com>
2024-04-18 22:11:37 +0530

    updates in email templates

--- emailTemplates/orderConfirmed.js
+++ emailTemplates/orderConfirmed.js
@@ -1,20 +1,43 @@
 const confirmOrder = (orderNumber, customerName, orderDate, productsData) => {
-  const productItems = productsData.map(({ productName, productQuantity, productSize, productDetails, productPrice, productImage }) => {
-    return `
-      <tr style="border-bottom: 1px solid #b6b6b6; font-size: 14px; color: #24104f;">
-        <td style="padding: 10px; text-align: left; display: flex; align-items: center;">
+  const productItems = productsData
+    .map(
+      ({
+        productName,
+        productQuantity,
+        productSize,
+        productDetails,
+        productPrice,
+        productImage,
+      }) => {
+        return `
+      <tr style="border-bottom: 1px solid rgba(182, 182, 182, 0.3);
+      font-size: 14px;
+      color: #24104f;">
+        <td style="padding: 10px;
+        text-align: left;
+        display: flex;
+        align-items: center;">
           <span>${productQuantity}</span>
-          <span style="margin-left: 20px;">
-            <img src="${productImage}" alt="product" style="width: 50px; height: auto;">
+          <span style="margin-left: 20px">
+            <img src="${productImage}" alt="${productName}" style="width: 50px; height: auto;">
           </span>
         </td>
         <td style="padding: 10px; text-align: left;">${productSize}</td>
         <td style="padding: 10px; text-align: left;">${productDetails}</td>
-        <td style="padding: 10px; text-align: right;">R${productQuantity * parseInt(productPrice)}</td>
+        <td style="padding: 10px; text-align: right;">R${
+          productQuantity * parseInt(productPrice)
+        }</td>
       </tr>
     `;
-  }).join('');
-  const totalAmount = productsData.reduce((total, { productQuantity, productPrice }) => total + (productQuantity * parseInt(productPrice)), 0);
+      }
+    )
+    .join("");
+
+  const totalAmount = productsData.reduce(
+    (total, { productQuantity, productPrice }) =>
+      total + productQuantity * parseInt(productPrice),
+    0
+  );
 
   return `<!DOCTYPE html>
     <html lang="en">
@@ -24,12 +47,12 @@ const confirmOrder = (orderNumber, customerName, orderDate, productsData) => {
         <title>Order Confirmation</title>
       </head>
       <body>
-        <section style="max-width: 800px; margin: 0 auto; padding: 20px; font-family: Arial, sans-serif;">
+        <div style="max-width: 800px; margin: 0 auto; padding: 10px; font-family: Arial, sans-serif;">
           <div style="text-align: center; display: flex; justify-content: center">
             <img src="https://res.cloudinary.com/dtb0waros/image/upload/v1713083989/brand-logo_ka5idp.png" alt="brand-logo" style="max-width: 200px; height: auto">
           </div>
-          <h1 style="text-align: center; font-size: 24px; margin: 20px 0; background: linear-gradient(94.29deg, #24104f 7.3%, #6551f3 119.44%); -webkit-background-clip: text; -webkit-text-fill-color: transparent;">
-            Your order <strong>${orderNumber}</strong><br />has been received.
+          <h1 style="text-align: center; font-size: 24px; font-weight: 400; margin: 20px 0; color: #5945E3;">
+            Your order <strong>#${orderNumber}</strong><br />has been received.
           </h1>
           <div style="border: 1px solid #d4ceff; border-radius: 10px; background: white; padding: 20px;">
             <p style="font-size: 16px; color: #24104f; line-height: 1.5">
@@ -38,7 +61,7 @@ const confirmOrder = (orderNumber, customerName, orderDate, productsData) => {
               We will email you whenever the status of your order changes.
             </p>
             <div style="margin-top: 20px">
-              <ul>
+              <ul style="padding: 0;">
                 <li style="display: flex; align-items: center">
                   <div>
                     <p style="font-weight: normal; font-size: 14px; color: black">
@@ -50,10 +73,10 @@ const confirmOrder = (orderNumber, customerName, orderDate, productsData) => {
                   </div>
                   <div style="margin-left: 20px">
                     <p style="font-weight: bold; font-size: 14px; color: black">
-                    ${orderNumber}
+                      #${orderNumber}
                     </p>
                     <p style="font-weight: bold; font-size: 14px; color: black">
-                    ${orderDate}
+                      ${orderDate}
                     </p>
                   </div>
                 </li>
@@ -62,42 +85,125 @@ const confirmOrder = (orderNumber, customerName, orderDate, productsData) => {
     
             <div style="margin-top: 20px">
               <h2 style="font-size: 20px; font-weight: bold; color: #24104f">
-                Here are details for your order. Order Details:
+                Here are details for your order.
               </h2>
-              <table style="width: 100%; border-collapse: collapse; margin-top: 10px;">
+              <table style="width: 100%; border-collapse: collapse; margin-top: 10px">
                 <thead>
-                  <tr style="border-bottom: 1px solid #b6b6b6; background: #d9d9d9; color: #24104f; font-size: 16px; text-align: left;">
-                    <th style="padding: 10px">Qty.</th>
-                    <th style="padding: 10px">Size</th>
-                    <th style="padding: 10px">Details</th>
-                    <th style="padding: 10px; text-align: right">Price</th>
+                  <tr style="
+                        background-color: rgba(217, 217, 217, 0.2);
+                        font-size: 16px;
+                        font-weight: 700;
+                        height: 54px;
+                        text-align: left;
+                      ">
+                    <th style="padding: 10px;">Qty.</th>
+                    <th style="padding: 10px;">Size</th>
+                    <th style="padding: 10px;">Details</th>
+                    <th style="padding: 10px; text-align: right;">Price</th>
                   </tr>
                 </thead>
                 <tbody>
                   ${productItems}
                 </tbody>
               </table>
-              <div style="border-top: 1px solid #b6b6b6; padding: 20px; display: flex; justify-content: space-between; align-items: center; background: #d9d9d9;">
-                <p style="font-size: 16px; font-weight: bold; color: #24104f">Total amount</p>
-                <p style="font-size: 24px; font-weight: normal; background: linear-gradient(94.29deg, #24104f 7.3%, #6551f3 119.44%); -webkit-background-clip: text; -webkit-text-fill-color: transparent;">
-                  R${totalAmount}
-                </p>
+              <div style="
+                border-top: 1px solid rgba(182, 182, 182, 0.3);
+                padding: 20px;
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                background-color: rgba(217, 217, 217, 0.2);
+              ">
+              <p style="font-size: 16px; font-weight: bold; color: #24104f">
+              Total amount
+            </p>
+              <p style="
+                    font-size: 24px;
+                    font-weight: normal;
+                    color: #5945E3;
+                  ">
+                R ${totalAmount}
+              </p>
               </div>
+              <div style="
+                border-top: 1px solid rgba(182, 182, 182, 0.3);
+                padding: 20px 0;
+                margin-top: 20px;
+              ">
+            <h2 style="font-size: 20px; font-weight: bold; color: #24104f">
+              To be delivered to:
+            </h2>
+            <p style="font-size: 16px; color: black; margin-top: 10px">
+              <strong>Kgosi Kekena</strong><br />
+              <strong>073 123 4567</strong>
+            </p>
+            <p style="font-size: 16px; color: #24104f; margin-top: 10px">
+              234 Street name,<br />
+              Area, <br />
+              1234
+            </p>
+          </div>
+
+          <div style="
+                border-top: 1px solid rgba(182, 182, 182, 0.3);
+                padding: 20px 0;
+                margin-top: 20px;
+              ">
+            <p style="font-size: 14px; color: #24104f">
+              Warm Regards,<br />Chelo Team
+            </p>
+            <div style="
+                  display: flex;
+                  justify-content: center;
+                  margin-top: 20px;
+                ">
+              <button style="
+                    padding: 10px 20px;
+                    background: hsl(248, 74%, 58%);
+                    color: white;
+                    font-weight: 500;
+                    font-size: 14px;
+                    border: none;
+                    border-radius: 5px;
+                    margin-right: 10px;
+                    cursor: pointer;
+                  ">
+                Track Your Order
+              </button>
+              <button style="
+                    padding: 10px 20px;
+                    background: transparent;
+                    color: black;
+                    font-weight: 500;
+                    font-size: 14px;
+                    border: 1px solid #5945e3;
+                    border-radius: 5px;
+                    cursor: pointer;
+                  ">
+                Download Receipt
+              </button>
             </div>
           </div>
+
           <div style="text-align: center; margin-top: 20px">
             <p style="font-size: 16px; color: #24104f">
               Don't want to receive these emails anymore?
               <a href="#" style="text-decoration: underline; color: #24104f">Unsubscribe</a>
             </p>
             <p style="font-size: 16px; color: #24104f">
-              <a href="#" style="text-decoration: underline; color: #24104f">Manage Your Profile</a> |
-              <a href="#" style="text-decoration: underline; color: #24104f">Terms</a> |
-              <a href="#" style="text-decoration: underline; color: #24104f">Code of Conduct</a> |
+              <a href="#" style="text-decoration: underline; color: #24104f">Manage Your Profile</a>
+              |
+              <a href="#" style="text-decoration: underline; color: #24104f">Terms</a>
+              |
+              <a href="#" style="text-decoration: underline; color: #24104f">Code of Conduct</a>
+              |
               <a href="#" style="text-decoration: underline; color: #24104f">Privacy Centre</a>
             </p>
           </div>
-        </section>
+            </div>
+            
+          </div>
+        </div>
       </body>
     </html>`;
 };
--- emailTemplates/orderDelivery.js
+++ emailTemplates/orderDelivery.js
@@ -10,6 +10,13 @@ const orderDelivery = (
           <meta charset="UTF-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <title>Order Delivery Ready</title>
+          <style>
+            @media screen and (max-width: 600px) {
+              .button-container {
+                display: block;
+              }
+            }
+          </style>
         </head>
         <body>
           <section
@@ -21,28 +28,16 @@ const orderDelivery = (
             "
           >
             <div style="text-align: center; display: flex; justify-content: center">
-            <a href="https://your-chelo-shop.com">
-                <img
+              <img
                 src="https://res.cloudinary.com/dtb0waros/image/upload/v1713083989/brand-logo_ka5idp.png"
                 alt="brand-logo"
                 style="max-width: 200px; height: auto"
-                />
-            </a>
+              />
             </div>
-            <h1 style="text-align: center; font-size: 24px; margin: 20px 0">
-              <span
-                style="
-                  background: linear-gradient(
-                    94.29deg,
-                    #24104f 7.3%,
-                    #6551f3 119.44%
-                  );
-                  -webkit-background-clip: text;
-                  -webkit-text-fill-color: transparent;
-                "
-              >
-                Your order <strong>#${orderNumber}</strong><br />is on its way 😊
-              </span>
+            <h1 style="text-align: center; font-size: 24px; font-weight: 400; margin: 20px 0">
+              <span style="color: #5945E3;">
+                Your order <strong>#${orderNumber}</strong><br />is on its way
+              </span>😊<span style="color: #5945E3;">.</span>
             </h1>
             <div
               style="
@@ -54,62 +49,55 @@ const orderDelivery = (
             >
               <p style="font-size: 16px; color: #24104f; line-height: 1.5">
                 Hi ${customerName}, <br /><br />
-              We have great news! Your order ${orderNumber} has left our warehouse and is
-            on its way to your address. You can also keep track of your order
-            <a href="${trackingLink}" style="text-decoration: underline; color: #24104f">${trackingLink}</a
-            >.<br /><br />
-            Thank you for shopping at
-            <a href="https://your-chelo-shop.com" style="text-decoration: underline; color: #24104f"
-            >Chelo</a
-            >.
-
-            </p>
-            <div
-              style="
-                border-top: 1px solid #b6b6b6;
-                padding: 20px 0;
-                margin-top: 20px;
-              "
-            >
-              <h2 style="font-size: 20px; font-weight: bold; color: #24104f">
-                Delivery Address:
-              </h2>
-              <p style="font-size: 16px; color: black; margin-top: 10px">
-                <strong>${customerName}</strong><br />
-                <strong>${deliveryAddress.contactNumber}</strong>
-              </p>
-              <p style="font-size: 16px; color: #24104f; margin-top: 10px">
-              ${deliveryAddress.addressLine1},<br />${deliveryAddress.addressLine2},<br />${deliveryAddress.streetCode}
-              </p>
-            </div>
-            <div
-              style="
-                border-top: 1px solid #b6b6b6;
-                padding: 20px 0;
-                margin-top: 20px;
-              "
-            >
-              <h2 style="font-size: 20px; font-weight: bold; color: #24104f">
-                Need more info:
-              </h2>
-              <p style="font-size: 14px; color: #24104f; margin-top: 10px">
-                Contact DSV directly on <strong>011 671 9774</strong>. Use your ID
-                number or <br />the order number <strong>${orderNumber}</strong> as a
-                reference.
-              </p>
-              <p style="font-size: 14px; color: #24104f; margin-top: 20px">
-                Warm Regards,<br />Chelo Team
+                We have great news! Your order <strong>#${orderNumber}</strong> has left our warehouse and is
+                on its way to your address. You can also keep track of your order
+                <a href="${trackingLink}" style="text-decoration: underline; color: #24104f">here</a>.<br /><br />
+                Thank you for shopping at
+                <a href="https://your-chelo-shop.com" style="text-decoration: underline; color: #24104f">Chelo</a>.
               </p>
               <div
                 style="
+                  border-top: 1px solid rgba(182, 182, 182, 0.3);
+                  padding: 20px 0;
+                  margin-top: 20px;
+                "
+              >
+                <h2 style="font-size: 20px; font-weight: bold; color: #24104f">
+                  Delivery Address:
+                </h2>
+                <p style="font-size: 16px; color: black; margin-top: 10px">
+                  <strong>${customerName}</strong><br />
+                  <strong>${deliveryAddress.contactNumber}</strong>
+                </p>
+                <p style="font-size: 16px; color: #24104f; margin-top: 10px">
+                  ${deliveryAddress.addressLine1},<br />${deliveryAddress.addressLine2},<br />${deliveryAddress.streetCode}
+                </p>
+              </div>
+              <div
+                style="
+                  border-top: 1px solid rgba(182, 182, 182, 0.3);
+                  padding: 20px 0;
+                  margin-top: 20px;
+                "
+              >
+                <h2 style="font-size: 20px; font-weight: bold; color: #24104f">
+                  Need more info:
+                </h2>
+                <p style="font-size: 14px; color: #24104f; margin-top: 10px">
+                  Contact DSV directly on <strong>011 671 9774</strong>. Use your ID
+                  number or <br />the order number <strong>${orderNumber}</strong> as a
+                  reference.
+                </p>
+                <p style="font-size: 14px; color: #24104f; margin-top: 20px">
+                  Warm Regards,<br />Chelo Team
+                </p>
+                <div style="
                   display: flex;
                   justify-content: center;
                   margin-top: 20px;
                   flex-wrap: wrap;
-                "
-              >
-                <button
-                  style="
+                ">
+                  <button style="
                     padding: 10px 20px;
                     background: #5945e3;
                     color: white;
@@ -117,41 +105,30 @@ const orderDelivery = (
                     border-radius: 5px;
                     margin-right: 10px;
                     cursor: pointer;
-                  "
-                >
-                  Track Your Order
-                </button>
+                  ">
+                    Track Your Order
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-          <div style="text-align: center; margin-top: 20px">
-            <p style="font-size: 16px; color: #24104f">
-              Don't want to receive these emails anymore?
-              <a href="#" style="text-decoration: underline; color: #24104f"
-                >Unsubscribe</a
-              >
-            </p>
-            <p style="font-size: 16px; color: #24104f">
-              <a href="https://your-chelo-shop.com/login" style="text-decoration: underline; color: #24104f"
-                >Manage Your Profile</a
-              >
-              |
-              <a href="#" style="text-decoration: underline; color: #24104f"
-                >Terms</a
-              >
-              |
-              <a href="#" style="text-decoration: underline; color: #24104f"
-                >Code of Conduct</a
-              >
-              |
-              <a href="#" style="text-decoration: underline; color: #24104f"
-                >Privacy Centre</a
-              >
-            </p>
-          </div>
-        </section>
-      </body>
-    </html>`;
+            <div style="text-align: center; margin-top: 20px">
+              <p style="font-size: 16px; color: #24104f">
+                Don't want to receive these emails anymore?
+                <a href="#" style="text-decoration: underline; color: #24104f">Unsubscribe</a>
+              </p>
+              <p style="font-size: 16px; color: #24104f">
+                <a href="#" style="text-decoration: underline; color: #24104f">Manage Your Profile</a>
+                |
+                <a href="#" style="text-decoration: underline; color: #24104f">Terms</a>
+                |
+                <a href="#" style="text-decoration: underline; color: #24104f">Code of Conduct</a>
+                |
+                <a href="#" style="text-decoration: underline; color: #24104f">Privacy Centre</a>
+              </p>
+            </div>
+          </section>
+        </body>
+      </html>`;
 };
 
 export { orderDelivery };
